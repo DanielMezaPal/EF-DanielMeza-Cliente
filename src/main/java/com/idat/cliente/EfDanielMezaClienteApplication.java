df633c6ec6e52f9d5c0ea36acf895ec3b2088bd7
Daniel Meza Palacios <ir.danhyel@gmail.com>
2022-08-17 00:01:26 -0500

    Inicio

---
+++ src/main/java/com/idat/cliente/EfDanielMezaClienteApplication.java
@@ -0,0 +1,13 @@
+package com.idat.cliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfDanielMezaClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfDanielMezaClienteApplication.class, args);
+	}
+
+}
